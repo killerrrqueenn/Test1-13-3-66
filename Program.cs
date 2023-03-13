62939e1fd124f79861727088623166fef0932753
win <muhaimin2547@gmail.com>
2023-03-13 17:26:30 +0700

    Finnish

--- Program.cs
+++ Program.cs
@@ -1,2 +1,104 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿ class Program {
+    static void Main(string[] args) {
+
+        
+        Console.WriteLine("Please Input Day of week : "); // Day 1-7 only
+       int Day = int.Parse(Console.ReadLine());
+       if (Day == 1) {
+       Console.WriteLine("monday");
+       }
+       else if (Day == 2) {
+        Console.WriteLine("tuesday");
+       }
+        else if (Day == 3) {
+        Console.WriteLine("wednesday");
+        }
+        else if (Day == 4) {
+        Console.WriteLine("thursday");
+        }
+        else if (Day == 5) {
+        Console.WriteLine("friday");
+        }
+        else if (Day == 6) {
+        Console.WriteLine("saturday");
+        }
+        else if (Day == 7) {
+        Console.WriteLine("sunday");
+        }
+        
+        Console.WriteLine("Please Input Time : "); // Time 1-18 only
+       int Time = int.Parse(Console.ReadLine());
+
+       if (Time == 1) {
+        Console.WriteLine("01.00 am");
+       }
+       else if (Time == 2) {
+        Console.WriteLine("02.00 am");
+       }
+        else if (Time == 3) {
+        Console.WriteLine("03.00 am");
+        }
+        else if (Time == 4) {
+        Console.WriteLine("04.00 am");
+        }
+        else if (Time == 5) {
+        Console.WriteLine("05.00 am");
+        }
+        else if (Time == 6) {
+        Console.WriteLine("06.00 am");
+        }
+        else if (Time == 7) {
+        Console.WriteLine("07.00 am");
+        }
+        else if (Time == 8) {
+         Console.WriteLine("08.00 am");
+       }
+       else if (Time == 9) {
+        Console.WriteLine("09.00 am");
+       }
+        else if (Time == 10) {
+        Console.WriteLine("10.00 am");
+        }
+        else if (Time == 11) {
+        Console.WriteLine("11.00 am");
+        }
+        else if (Time == 12) {
+        Console.WriteLine("12.00 pm");
+        }
+        else if (Time == 13) {
+        Console.WriteLine("13.00 pm");
+        }
+        else if (Time == 14) {
+        Console.WriteLine("14.00 pm");
+        }
+         else if (Time == 15) {
+        Console.WriteLine("15.00 pm");
+        }
+        else if (Time == 16) {
+        Console.WriteLine("16.00 pm");
+        }
+        else if (Time == 17) {
+        Console.WriteLine("17.00 pm");
+        }
+        else if (Time == 18) {
+        Console.WriteLine("18.00 pm");
+        }
+
+      Console.WriteLine(" Input Menu : "); 
+       int Menu = int.Parse(Console.ReadLine());
+
+        if (Menu == 1) {
+        Console.WriteLine("Breakfast Set");
+        }
+        if (Menu == 2) {
+        Console.WriteLine("Weekend Set");
+       }
+         if (Menu == 3) {
+        Console.WriteLine("Coffee");
+
+    }
+ }
+ }
+    
+        
+       
